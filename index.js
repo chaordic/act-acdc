878ace11cd9c87a65702e53cf697936f01eee999
Thiago Caiubi <thiago.caiubi@gmail.com>
2014-05-08 20:04:54 -0300

    Add Redis to cache Twitter search result

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var restify = require("restify"),
     server = restify.createServer(),
 
     twitterAuth,
-    twitter;
+    twitter,
+
+    redis = require("redis").createClient();
 
 try {
     twitterAuth = require("./twitterAuth");
@@ -15,11 +17,41 @@ try {
 twitter = new require("twitter")(twitterAuth);
 
 server.get("/", function(req, res, next) {
-    twitter.search("#acdc", function(data) {
-        res.send(data);
-        next();
-    });
+    var term = "#acdc",
+        key = "twitter-search-" + term;
+
+    cache.get(key,
+        function miss(){
+            twitter.search(term, function(data) {
+                cache.set(key, data);
+                res.send(data);
+                next();
+            });
+        },
+        function hit(data){
+            res.send(data);
+            next();
+        });
 });
+
+var cache = {
+    get: function(key, miss, hit) {
+        redis.get(key, function(err, data) {
+            if (err || !data) {
+                miss();
+            } else {
+                hit(JSON.parse(data));
+            }
+        });
+    },
+    set: function(key, data) {
+        redis.
+            multi().
+            set(key, JSON.stringify(data)).
+            pexpire(key, 300000).
+            exec();
+    }
+};
 
 server.listen(8080, function() {
     console.log("AC/DC is up and running!");
