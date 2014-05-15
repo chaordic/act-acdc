aafc6360ef9624221cf20b4e7aa4ff9a4ab906ef
Thiago Caiubi <thiago.caiubi@gmail.com>
2014-05-15 12:57:24 -0300

    Post request elapsed time and request count to Librato

--- act-acdc.js
+++ act-acdc.js
@@ -1,8 +1,24 @@
 var server = require("restify").createServer(),
 
-    index = require("./lib/index");
+    index = require("./lib/index"),
+    librato = require("./lib/librato");
+
+server.pre(function(req, res, next) {
+    req.actAcdc = {
+        startTime: new Date().getTime()
+    };
+
+    next();
+});
 
 server.get("/", index);
+
+server.on("after", function(req) {
+    var elapsedTime = new Date().getTime() - req.actAcdc.startTime;
+
+    librato.increment("act-acdc.request.time", elapsedTime);
+    librato.increment("act-acdc.request.count");
+});
 
 server.listen(8080, function() {
     console.log("AC/DC is up and running!");
